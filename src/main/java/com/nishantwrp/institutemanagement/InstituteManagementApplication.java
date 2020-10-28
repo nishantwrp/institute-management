2451ef3382aefac4b1730199880ac73ac1b452f9
Nishant Mittal <mittalnishant14@outlook.com>
2020-10-29 02:30:32 +0530

    Initialized Spring

---
+++ src/main/java/com/nishantwrp/institutemanagement/InstituteManagementApplication.java
@@ -0,0 +1,13 @@
+package com.nishantwrp.institutemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstituteManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstituteManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nishantwrp/institutemanagement/InstituteManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nishantwrp.institutemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstituteManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
